ce8e0403ecf7c12199359397552cb85700a039c1
Sarffy Geri <sarffygeri@gmail.com>
2021-11-06 14:40:38 +0100

    cursor position script

---
+++ cursor.py
@@ -0,0 +1,9 @@
+#import pyautogui
+import pyautogui
+
+#print the position of the mouse continously
+while True:
+    x, y = pyautogui.position()
+    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+    print(positionStr, end='')
+    print('\b' * len(positionStr), end='', flush=True)
